d8ca9f3f141a3327d8ed4d14619fc1804aacd688
nic rose <nic_rose@taylor.edu>
2019-08-01 20:27:09 -0400

    added sorting flags

--- AssigmentTracker/assignmentTracker.py
+++ AssigmentTracker/assignmentTracker.py
@@ -5,16 +5,32 @@ def options():
     print("2. Show assignments")
     print("3. Delete assignment")
     print("4. Edit assignmment")
+    print("5. Complete assignment")
     print("Exit. To stop the program")
     option = input("choose the number that needs done ")
     return option
 
 def addAssignment(assignments):
-    info = input("Give info about assignment: day its due, hours to complete, class, Name (ex. 9/2/19 1 Cos120 L01) ")
+    info = input("Give info about assignment: day its due, when it is due, hours to complete, class, Name (ex. 9/2/19 1 Cos120 L01) ")
     infolst = info.split()
     assignments.append(infolst)
     
+def sortAssignments(assignments):
+    flag = input("What would you like to sort by? ")
+    if flag in ["Date", "date", "d"]:
+        assignments.sort(key=lambda x: x[0])
+    if flag in ["s", "short"]:
+        assignments.sort(key=lambda x: x[2])
+    if flag in ["l", "long"]:
+        assignments.sort(key=lambda x: x[2], reverse = True)
+    if flag in ["class", "c"]:
+        assignments.sort(key=lambda x: x[3])
+    if flag in ["name", "n", "assignment", "a"]:
+        assignments.sort(key=lambda x: x[4])
+        
 def showAssignments(assignments):
+    sortAssignments(assignments)
+    print()
     for i in assignments:
         aStr = ""
         for j in i:
@@ -34,8 +50,7 @@ def editAssignment(assignments):
     deleteAssignment(assignments)
     addAssignment(assignments)
 
-def main():
-    exitOptions = ["exit", "Exit", "e", "quit", "Quit", "q"]
+def main(): 
     infile = open("assignments.txt", "r")
     assignments = []
     for i in infile:
@@ -52,8 +67,10 @@ def main():
         if option == "3":
             deleteAssignment(assignments)
         if option == "4":
-            editAssignment(assignment)
-        if option in exitOptions:
+            editAssignment(assignments)
+        if option == "5":
+            completeAssignment(assignments)
+        if option in ["exit", "Exit", "e", "quit", "Quit", "q"]:
             print("Exiting")
             break
 
@@ -61,7 +78,6 @@ def main():
 
 
     outfile = open("assignments.txt", "w")
-    assignments.sort()
     for i in assignments:
         for j in i:
             outfile.write(j + " ")
